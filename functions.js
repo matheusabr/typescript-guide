81ceede89167d3d5ed4c7559ef444bc321b32ee3
Matheus Abreu <mattabreu@Matheuss-MacBook-Air.local>
2017-12-21 00:57:02 -0300

    chore(): functions

---
+++ functions.js
@@ -0,0 +1,11 @@
+// JS function
+var logJS = function (message) {
+    console.log(message);
+};
+// TS Arrow functions
+var logTS = function (message) {
+    console.log(message);
+};
+// or 1 line instruction
+// similar to C# and Java lambda expressions
+var alternativeLogTS = function (message) { return console.log(message); };
---
+++ functions.ts
@@ -0,0 +1,12 @@
+// JS function
+let logJS = function(message) {
+    console.log(message);    
+}
+
+// TS Arrow functions
+let logTS = (message) => {
+    console.log(message);
+}
+// or 1 line instruction
+// similar to C# and Java lambda expressions
+let alternativeLogTS = (message) => console.log(message);
